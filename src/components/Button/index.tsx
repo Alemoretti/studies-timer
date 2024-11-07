a00cbf1c286d831c5dee78d800e9e99d6ab74f9d
Alemoretti <alemoretti10@gmail.com>
2024-11-06 23:40:09 -0300

    Add task as state of the component and set button type

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,13 +2,15 @@ import React, { ReactNode } from 'react';
 import style from './Button.module.scss';
 
 interface ButtonProps {
-    children: ReactNode;
+    children: ReactNode,
+    type?: "button" | "submit" | "reset" | undefined
 }
 
 class Button extends React.Component<ButtonProps> {
     render() {
+        const { type = "button" } = this.props; 
         return (
-            <button className={style.button}>
+            <button className={style.button} type={type}>
                 {this.props.children}
             </button>   
         );
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,15 +3,27 @@ import Button from '../Button'
 import style from './Form.module.scss'
 
 class Form extends React.Component {
+  state = {
+    task: "",
+    time: "00:00"
+  }
+
+  addTask(event: React.FormEvent<HTMLFormElement>) {
+   event.preventDefault()
+   console.log(this.state)
+  }
+
   render() {
     return (
-      <form className={style.newTask}>
+      <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="task">Add a new study</label>
           <input 
             type="text"
             name="task"
             id="task"
+            value={this.state.task}
+            onChange={event => this.setState({...this.state, task: event.target.value})}
             placeholder="What do you want to study?"
             required
           />
@@ -23,13 +35,15 @@ class Form extends React.Component {
             step="1"
             name="time"
             id="time"
+            value={this.state.time}
+            onChange={event => this.setState({...this.state, time: event.target.value})}
             min="00:00:00"
             max="01:30:00"
             required
           />
         </div>
         
-        <Button>
+        <Button type="submit">
           Add
         </Button>
       </form>
